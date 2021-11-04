815e38da3246f83348ecb594ac52d9a2f504b4fe
trm621 <t.maher621@gmail.com>
2021-11-04 16:33:49 -0400

    commented out broken functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,12 +6,12 @@ var divEl = document.createElement("div");
 var searchHistory = document.getElementById("#history")
 var listItem = document.createElement("li");
 
-var displayCurrentWeather = function(cityName) {
+// var displayCurrentWeather = function(cityName) {
 
-    listItem.innerHTML = cityName;
-    searchHistory.appendChild(listItem)
-    listItem.setAttribute("style", "font-size:30px; text-align:center;")
-}
+//     listItem.textContent = cityName;
+//     searchHistory.appendChild(listItem)
+//     listItem.setAttribute("style", "font-size:30px; text-align:center;")
+// }
 
 var submitCity = function(event) {
     event.preventDefault();
@@ -22,7 +22,7 @@ var submitCity = function(event) {
     if (cityName) {
         getCurrentWeather(cityName);
         getFiveDayForecast(cityName);
-        displayCurrentWeather(cityName);
+        // displayCurrentWeather(cityName);
     }
     else {
         alert("Please input a city.")
